de7a299b73b9499630439883dcca16016af28c93
RNaveen98 <151598723+RNaveen98@users.noreply.github.com>
2024-11-03 05:44:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+  (function() {
+        let cx = '005383125436438536544:y1edweedxwi';
+        let gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        let s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
